29279d932fca7807641ef1b80ef6be35738c44a2
Paul Austdal <paul@austdal.net>
2023-04-10 11:24:00 +0000

    Moved vsconfig

---
+++ .devcontainer/tools.sh
@@ -0,0 +1,6 @@
+# HTTPie
+sudo apt-get update
+sudo apt-get -y install httpie
+
+# Grype
+wget -qO - https://raw.githubusercontent.com/anchore/grype/main/install.sh | sudo bash -s -- -b /usr/local/bin
